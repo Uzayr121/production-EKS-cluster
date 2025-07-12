1aeb41845778ac23b6a64b97f0e7b58e5e69c898
uzayr121 <uzayr121.uk@gmail.com>
2025-07-12 15:35:05 +0100

    adding helm charts and values

---
+++ Terraform/Module/Helm/variables.tf
@@ -0,0 +1,34 @@
+variable "cert_manager_name" {
+  description = "The name of the cert-manager Helm release."
+  type        = string
+  
+}
+
+variable "cert_manager_irsa_arn" {
+description = "The ARN of the cert-manager IRSA role."
+type = string
+
+}
+
+
+variable "external_dns_name" {
+
+description = "The name of the external-dns Helm release."
+type = string
+}
+
+variable "external_dns_irsa_arn" {
+description = "The ARN of the external-dns IRSA role."
+type = string
+
+}
+variable "nginx_ingress_name" {
+description = "The name of the NGINX Ingress controller Helm release."
+type = string
+}
+variable "aws_region" {
+  description = "The AWS region where the resources will be deployed."
+  type        = string
+  default = "eu-west-2"
+  
+}
